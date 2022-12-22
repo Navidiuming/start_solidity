a42efa18d9e419c81b8dc540bc93f2d3bc1f1024
Navidiuming <navid.abolfathi@gmail.com>
2022-12-22 13:39:48 +0330

    make  write function

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 contract MyContract{
-    
+    string myMood;
+
+    function setMood(string memory _mood) public {
+        myMood = _mood;
+    }
 }
